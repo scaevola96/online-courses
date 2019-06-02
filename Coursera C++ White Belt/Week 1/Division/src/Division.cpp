35ab4499281e2d80f948b9c7f2d7687256609e74
Sergey Barseghyan <sergey.barseghyan06@gmail.com>
2019-06-02 23:09:32 +0300

    Week 1

---
+++ Coursera C++ White Belt/Week 1/Division/src/Division.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int a,b;
+	cin>> a >> b ;
+	if(b==0){
+		cout<<"Impossible";
+	}
+
+	else{
+
+		cout<<a/b;
+	}
+
+	return 0;
+}
---
+++ Coursera C++ White Belt/Week 1/Equation/src/equation.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	double a,b,c;
+	cin>>a >>b >>c;
+
+	if(a==0){
+		if(b!=0){
+		 cout<<-c/b;
+		}
+		else{
+		 cout<<"";
+		}
+	}
+
+	else{
+			if(b*b-4*a*c>0){
+				cout<<(-b+sqrt(b*b-4*a*c))/(2*a) <<" "<< (-b-sqrt(b*b-4*a*c))/(2*a);
+			}
+			else if(b*b-4*a*c==0){
+				cout<<(-b+sqrt(b*b-4*a*c))/(2*a);
+			}
+
+	}
+	return 0;
+}
+
