c3d32643ea2e2f39cce26f304bfb9d864730c87f
Sergey Barseghyan <sergey.barseghyan06@gmail.com>
2019-06-08 21:56:49 +0300

    Week 2 part 3 problem 3

--- Coursera C++ White Belt/Week 2/Queue/src/Queue.cpp
+++ Coursera C++ White Belt/Week 2/Queue/src/Queue.cpp
@@ -8,41 +8,40 @@ string command; // command name
 int option; // command option
 vector<string> queue = {}; //queue
 vector<int> worrycount = {}; //
-void Come(vector<string>& q, const int option ){    // Command "Come"
-		if(option>0){
-			for(int i=0; i<option;i++){
 
-				q.push_back("QUIET");
+void Come(vector<string>& q, const int option ){    // Command "Come"
+	if(option>0){
+		for(int i=0; i<option;i++){
+			q.push_back("QUIET");
 
 			}
 		}
-		else if(option<0){
+	else if(option<0){
 
-			for(int i=0; i>option;i--){
-				q.pop_back();
+		for(int i=0; i>option;i--){
+			q.pop_back();
 
 			}
 		}
 }
 
 void Quite(vector<string>& q, const int option ){  		// Command "Quite"
-
 	q[option]="QUIET";
 }
 
 void Worry(vector<string>& q, const int option ){  		// Command "Worry"
 
 	q[option]="WORRY";
-
 }
+
 void WorryCount(vector<string>& q){  		// Command "Worry"
-		int cnt=0;
-			for(auto a: q){
-				if(a=="WORRY"){
-					cnt++;
-				}
+	int cnt=0;
+		for(auto a: q){
+			if(a=="WORRY"){
+				cnt++;
 			}
-		worrycount.push_back(cnt);
+		}
+	worrycount.push_back(cnt);
 }
 
 void CommandInput(){
@@ -50,22 +49,25 @@ void CommandInput(){
 		opnumber--;
 }
 
-
-void CheckCommand(const string checkcommand){ // Command Checker
+void CheckCommand(const string checkcommand){
+ // Command Checker
 	if(checkcommand=="COME"){
 		cin>>option;
 		Come(queue, option);
 		}
+
 	else if(checkcommand=="QUIET"){
 		cin>>option;
 		Quite(queue, option);
 
 	}
+
 	else if(checkcommand=="WORRY"){
 		cin>>option;
 		Worry(queue, option);
 
 		}
+
 	else if(checkcommand=="WORRY_COUNT"){
 			WorryCount(queue);
 
